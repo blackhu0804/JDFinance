06a30d1afd21d93dbfd832a324843e3b1f1a9758
hu970804 <812510003@qq.com>
2018-08-15 22:47:56 +0800

    add hslider and some components

--- app/js/home/index.vue
+++ app/js/home/index.vue
@@ -1,17 +1,22 @@
 <template lang="html">
     <div :class="$style.home">
-        <h1>Home</h1>
-        <p>abcd</p>
+        <Heador/>
+        <Slider/>
     </div>
 </template>
 
 <script>
+import Heador from "../public/header.vue"
+import Slider from "./hslider.vue"
 export default {
+    components: {
+        Heador,
+        Slider,
+    },
 }
 </script>
 
 <style lang="scss" module>
-  @import '../../css/reset.scss';
   .home{
     color: red;
     font-size: 40px;
